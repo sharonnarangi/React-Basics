4a1957135449ce8c49fb789210a2fdcd98b64abd
Sharon Nrangi <sharonnarangi@gmail.com>
2024-09-27 12:55:25 +0300

    React basics

--- react.js
+++ react.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-
-
 class HelloReact extends React.Component {    render () {      
       return (      <div>Hello React!</div>        ) 
 
    }}ReactDOM.render(<HelloReact/>, document.getElementById('app'))
+   //Render is a method.
+   //JSX extension
+
+   //Rendering Child Nodes using the map function
+
+   //map comes handy when building React components
